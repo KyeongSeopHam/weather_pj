0b1fb1d1e9756507fdf127802fc4985385500c97
KyeongSeopHam <rudtjq5724@gmail.com>
2022-10-18 05:49:06 +0900

    추가 : JPA방식을 활용으로 데이터저장해보기

--- src/main/java/nab_2/weather/domain/Memo.java
+++ src/main/java/nab_2/weather/domain/Memo.java
@@ -1,12 +1,23 @@
 package nab_2.weather.domain;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "memo")
 public class Memo {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
    private int id;
    private String text;
 }
